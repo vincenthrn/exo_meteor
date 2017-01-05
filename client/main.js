305342272f243b8cfdb751662dffad477e09922d
Vincent <vincent.henrion@hetic.net>
2017-01-05 16:22:26 +0100

    delete

--- client/main.js
+++ client/main.js
@@ -3,23 +3,39 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-
+/*
+ * Liste les élèves
+ * */
 Template.student.helpers({
     toutlemonde() {
         return eleves.find();
     },
 });
 
+/*
+* Ajoute un nouvel élève
+* */
 Template.student.events({
     "click #add_eleve, submit .add_form": function (event, template) {
 
         event.preventDefault();
         eleve_name = template.find("#nom_eleve").value;
 
-        eleves.insert({
-          name: eleve_name
-        });
+        if(eleve_name.length !== 0){
+            eleves.insert({
+                name: eleve_name
+            });
+        }
     }
 });
 
+/*
+ * Supprime un élève
+ * */
+Template.student.events({
+    "click .delete_eleve": function (event, template) {
 
+        event.preventDefault();
+        eleves.remove(this._id);
+    }
+});
